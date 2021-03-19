676ea73732575cff05343076c08fed7b7971ee32
sergeykaluzhskiy <arsuxa12345678910ee@gmail.com>
2021-03-19 16:31:33 +0300

    Добавьте файлы проекта.

---
+++ Krug/Krug.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+const double pi = 3.14;
+
+double CircleLenght(double radius)
+{
+	return 2.0 * pi * radius;
+}
+
+// Возвращает площадь окружности радиусом radius
+double CircleArea(double radius)
+{
+	return pi * radius * radius;
+}
+
+int main()
+{
+	double radius;
+
+	cout << "Enter radius of circe: ";
+	cin >> radius;
+
+	cout << "Lenght of circle = " << CircleLenght(radius) << std::endl;
+	cout << "Area of circle = " << CircleArea(radius) << std::endl;
+
+	return 0;
+}
